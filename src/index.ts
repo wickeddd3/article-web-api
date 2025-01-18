c1f863a482262a3e00942ec5ac43e820fa5597f4
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 11:55:44 +0800

    feat: set controllers to main App

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import 'dotenv/config';
 import 'module-alias/register';
 import { validateEnv } from '@/lib/validate-env';
 import { App } from '@/app';
+import { CompaniesController } from '@/modules/company/companies.controller';
+import { UsersController } from '@/modules/user/users.controller';
+import { ArticlesController } from '@/modules/article/articles.controller';
+import { AuthController } from '@/modules/auth/auth.controller';
 
 validateEnv();
 
-const app = new App([], Number(process.env.PORT));
+const app = new App(
+  [new CompaniesController(), new UsersController(), new ArticlesController(), new AuthController()],
+  Number(process.env.PORT),
+);
 
 app.listen();

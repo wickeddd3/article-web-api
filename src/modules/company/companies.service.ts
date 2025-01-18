4102b7cd81c5dc78c827c91ba91f6fbda6ddd792
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 06:14:31 +0800

    feat: upload logo to cloudinary and save secure_url response

--- src/modules/company/companies.service.ts
+++ src/modules/company/companies.service.ts
@@ -1,6 +1,14 @@
+import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } from '@/config/cloudinary-keys';
 import { CompaniesRepository } from '@/modules/company/companies.repository';
-import { CompanySchemaType } from '@/modules/company/company.schema';
+import { CompanySchemaType, EditCompanySchemaType } from '@/modules/company/company.schema';
 import { Company } from '@prisma/client';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
 
 export class CompaniesService {
   private companiesRepository = new CompaniesRepository();
@@ -25,16 +33,43 @@ export class CompaniesService {
 
   public async create(data: CompanySchemaType): Promise<Company | Error> {
     try {
-      const company = await this.companiesRepository.create(data);
+      let logoUrl: string | undefined;
+      if (data.logo) {
+        const uploadResponse = await cloudinary.uploader.upload(data.logo);
+        if (!uploadResponse.secure_url) {
+          throw new Error('Error occurred while uploading company logo');
+        }
+        logoUrl = uploadResponse.secure_url;
+      }
+      const dataRequest = {
+        ...data,
+        logo: logoUrl || '',
+      };
+
+      const company = await this.companiesRepository.create(dataRequest);
       return company as Company;
     } catch (error: any) {
       throw new Error(error.message);
     }
   }
 
-  public async update(id: number, data: CompanySchemaType): Promise<Company | Error> {
+  public async update(id: number, data: EditCompanySchemaType): Promise<Company | Error> {
     try {
-      const company = await this.companiesRepository.update(id, data);
+      let logoUrl: string | undefined;
+      if (data.newLogo) {
+        const uploadResponse = await cloudinary.uploader.upload(data.newLogo);
+        if (!uploadResponse.secure_url) {
+          throw new Error('Error occurred while uploading company logo');
+        }
+        logoUrl = uploadResponse.secure_url;
+      }
+      const dataRequest = {
+        name: data.name,
+        status: data.status,
+        ...(data.newLogo ? { logo: logoUrl } : {}),
+      };
+
+      const company = await this.companiesRepository.update(id, dataRequest);
       return company as Company;
     } catch (error: any) {
       throw new Error(error.message);

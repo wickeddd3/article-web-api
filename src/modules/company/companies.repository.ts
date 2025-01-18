ec1fe78570afb7bd9a8b221f620208238621e1a4
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-18 11:43:49 +0800

    feat: add companies repository

---
+++ src/modules/company/companies.repository.ts
@@ -0,0 +1,66 @@
+import { prisma } from '@/lib/prisma';
+import { CompanySchemaType } from '@/schemas/company.schema';
+import { Company } from '@prisma/client';
+
+export class CompaniesRepository {
+  private db = prisma;
+
+  public async list(): Promise<Company[] | Error> {
+    try {
+      const companies = await this.db.company.findMany();
+      return companies as Company[];
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async get(id: number): Promise<Company | Error> {
+    try {
+      const company = await this.db.company.findUnique({
+        where: { id },
+      });
+      return company as Company;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async create(data: CompanySchemaType): Promise<Company | Error> {
+    try {
+      const company = await this.db.company.create({
+        data: {
+          ...data,
+        },
+      });
+      return company as Company;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async update(id: number, data: CompanySchemaType): Promise<Company | Error> {
+    try {
+      const company = await this.db.company.update({
+        where: { id },
+        data: {
+          ...data,
+        },
+      });
+      return company as Company;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+
+  public async delete(id: number): Promise<void | Error> {
+    try {
+      await this.db.company.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  }
+}

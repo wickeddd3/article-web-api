88aa6275a60ee8c92103f7c2eceefa46cc5c70ad
wickeddd3 <pablophilipandrew@gmail.com>
2025-01-19 17:29:52 +0800

    feat: update ArticleSchema date and editorId validation. And add editArticleSchema

--- src/modules/article/article.schema.ts
+++ src/modules/article/article.schema.ts
@@ -4,12 +4,31 @@ export const articleSchema = z.object({
   image: z.string(),
   title: z.string().max(150),
   link: z.string().url(),
-  date: z.date(),
+  date: z
+    .string()
+    .nonempty('Date is required')
+    .refine((value) => !isNaN(Date.parse(value)), 'Invalid date format'),
   content: z.string(),
   status: z.enum(['ForEdit', 'Published']),
   writerId: z.number(),
-  editorId: z.number(),
+  editorId: z.number().optional(),
   companyId: z.number(),
 });
 
 export type ArticleSchemaType = z.infer<typeof articleSchema>;
+
+export const editArticleSchema = z.object({
+  newImage: z.string().optional(),
+  title: z.string().max(150),
+  link: z.string().url(),
+  date: z
+    .string()
+    .nonempty('Date is required')
+    .refine((value) => !isNaN(Date.parse(value)), 'Invalid date format'),
+  content: z.string(),
+  status: z.enum(['ForEdit', 'Published']),
+  editorId: z.number(),
+  companyId: z.number(),
+});
+
+export type EditArticleSchemaType = z.infer<typeof editArticleSchema>;
